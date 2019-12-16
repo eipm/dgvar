2676b430e13f46de852beb08e8477d9917ec502a
Tuo Zhang <taz2008@nice.pbtech>
2019-12-16 16:36:24 -0500

    clean codes for extracting candidate variants, in particular, combine multiple python scripts into a single one script

--- src/select_candidate_variants.py
+++ src/select_candidate_variants.py
@@ -26,7 +26,7 @@ outfile = sys.argv[3]
 exaccut = 0.01
 ####caddcut = 15
 ####outcols = ["Category","#CHROM","POS","ID","REF","ALT","FILTER","VariantType","FS","GT","AlleleFreq","SnpEffEffect","SnpEffGeneName","SnpEffHGVS.c","SnpEffHGVS.p","exacAdjAF","clinvarHits","Hugo_Symbol","Chromosome","Start_Position","End_Position","Strand","Variant_Classification","Variant_Type","Reference_Allele","Tumor_Seq_Allele1","Tumor_Seq_Allele2","HGVSc","HGVSp","HGVSp_Short","Transcript_ID"]
-outcols = ["Category","#CHROM","POS","ID","REF","ALT","FILTER","VariantType","GT","AlleleFreq","SnpEffEffect","SnpEffGeneName","SnpEffFeatureID","SnpEffHGVS.c","SnpEffHGVS.p","exacAdjAF","clinvarHits"]
+outcols = ["Category","#CHROM","POS","ID","REF","ALT","FILTER","VariantType","GT","AlleleFreq","SnpEffEffect","SnpEffGeneName","SnpEffFeatureID","SnpEffHGVS.c","SnpEffHGVS.p","exacAdjAF","clinvarHits","COSMIC_Onco","COSMIC_TSG","UT_TSG"]
 
 # file exists?
 if not exists(infile):
@@ -39,9 +39,9 @@ fin.close()
 #print sid,":",len(variants)-1,"variants in total,",
 
 # screen candidates and output
-print "PMid\tnumTotal\tnumHits\tnumClvP\tnumPass\tnumQC\tnumExAC\tnumClvB\tnumImpact\tnumInbreed"
+print "PMid\tnumTotal\tnumHits\tnumClvP\tnumTrunc\tnumOnco\tnumQC\tnumExAC\tnumClvB\tnumInbreed"
 # screen
-numHits = numQC = numExAC = numClvP = numClvB = numImpact = numInbreed = numPass = 0
+numHits = numQC = numExAC = numClvP = numClvB = numTrunc = numInbreed = numOnco = 0
 fout = open(outfile,'wb')
 fout.write("Tumor_Sample_Barcode\t"+"\t".join(outcols)+"\n")
 hdic = {}# header dictionary
@@ -52,49 +52,44 @@ for k,elements in enumerate(variants):
 		continue
 	mark = True
 	category = elements[hdic["Category"]]
-	gname = elements[hdic["Hugo_Symbol"]]
+	gname = elements[hdic["SnpEffGeneName"]]
 	exac = [x for x in elements[hdic["exacAdjAF"]].split(",") if x != "." and x != "NA"]
-	#caddpass = elements[hdic["CADDGeneMatch"]].split(",")
-	#caddphred = elements[hdic["CADDPhredScore"]].split(",")
 	filt = elements[hdic["FILTER"]]
-	impact = elements[hdic["SnpEffImpact"]].split(",")
+	effect = elements[hdic["SnpEffEffect"]]
+	cosonco = elements[hdic["COSMIC_Onco"]]
+	costsg = elements[hdic["COSMIC_TSG"]]
+	uttsg = elements[hdic["UT_TSG"]]
 	# filter
 	if category == "C.F" or category == "F":
 		mark = False
 		numQC += 1
-	####if gname.upper() not in genelist:
-	####	mark = False
-	####if category == "P.S":# protein truncating + exac < 1%
-	####	mark = True
 	elif len(exac) != 0 and min([atof(x) for x in exac]) > exaccut:
 		mark = False
 		numExAC += 1
-	elif category == "P.C" or category == "LP.C":# annotated pathogenic/likely pathogenic variants by ClinVar
+	elif category in ["P.C","LP.C"] and (cosonco == "yes" or costsg == "yes" or uttsg == "yes"):# annotated pathogenic/likely pathogenic variants by ClinVar
 		mark = True
 		numClvP += 1
-	elif category == "B.C" or category == "LB.C":# annotated benign/likely benign variants by ClinVar
+	elif category in ["B.C","LB.C"]:# annotated benign/likely benign variants by ClinVar
 		mark = False
 		numClvB += 1
-	elif "HIGH" not in impact:
-		mark = False
-		numImpact += 1
 	elif "InbreedingCoeff_Filter" in filt or "VQSRTrancheINDEL" in filt or "VQSRTrancheSNP" in filt:# keep AC_Adj0_Filter
 		mark = False
 		numInbreed += 1
-	else:
-		numPass += 1
+	elif ("frameshift_variant" in effect or "stop_gained" in effect) and (costsg == "yes" or uttsg == "yes"):# protein truncating on TSG
+		mark = True
+		numTrunc += 1
+	elif cosonco == "yes":# non-protein truncating on onco gene
+		mark == True
+		numOnco += 1
 	# output
 	if mark:
 		numHits += 1
 		###fout.write(sid+"\t"+"\t".join([elements[hdic[x]] for x in outcols])+"\n")
 		fout.write(sid+"\t"+"\t".join([fix_splice_region(elements,hdic,x) for x in outcols])+"\n")# fix Splice_Region annotation
 fout.close()
-# double check
-if numHits != numClvP + numPass:
-	print "Weird",sid,numHits,numClvP,numPass
-	sys.exit(2)
+
 # PMid\tnumTotal\tnumHits\tnumClvP\tnumPass\tnumQC\tnumExAC\tnumClvB\tnumImpact\tnumInbreed
-print "%s\t%s"%(sid,"\t".join(["%d"%x for x in [len(variants)-1,numHits,numClvP,numPass,numQC,numExAC,numClvB,numImpact,numInbreed]]))
+print "%s\t%s"%(sid,"\t".join(["%d"%x for x in [len(variants)-1,numHits,numClvP,numTrunc,numOnco,numQC,numExAC,numClvB,numInbreed]]))
 
 #print "Complete!"
 
